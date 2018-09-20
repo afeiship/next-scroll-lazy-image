7c90b0afc6a620922b07b0702fe144860fb84ce2
afeiship <1290657123@qq.com>
2018-09-20 17:30:44 +0800

    issue: bugifx

--- src/next-scroll-lazy-image.js
+++ src/next-scroll-lazy-image.js
@@ -3,32 +3,58 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var DEFAULT_LAZY_IMAGE = '.react-lazy-image';
-  var NxDomEvent = nx.dom.Event || require('next-dom-event');
+  var NxDomEvent = (nx.dom && nx.dom.Event) || require('next-dom-event');
   var nxThrottle = nx.throttle || require('next-throttle');
+
+
+  //TODO: has bug:
+  function isInViewport(inElement, inOffset) {
+    var offset = inOffset || 0;
+    var box = inElement.getBoundingClientRect();
+    var viewport = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+    return !(box.bottom < 0 || box.top > viewport.height) && !(box.right < 0 || box.left > viewport.width);
+  }
 
   var NxScrollLazyImage = nx.declare('nx.ScrollLazyImage', {
     methods: {
-      init: function (inContext) {
+      init: function (inContext, inOffset, inInterval) {
         this.context = inContext || global.document;
-        this.elements = nx.slice(context.querySelectorAll(DEFAULT_LAZY_IMAGE));
+        this.interval = inInterval || 600;
+        this.offset = inOffset || 100;
+        this.elements = nx.slice(this.context.querySelectorAll(DEFAULT_LAZY_IMAGE));
         this.attachEvents();
+        this.trigger();
       },
       destroy: function () {
         this.detachEvents();
       },
       attachEvents: function () {
-        this._scrollRes = NxDomEvent.on(global, 'scroll', nxThrottle(this._onScroll, 300, this), this);
+        var handler = nxThrottle(this.trigger, this.interval, this);
+        this._scrollRes = NxDomEvent.on(global, 'scroll', handler, this);
       },
       detachEvents: function () {
         this._scrollRes.destroy();
         this.context = null;
         this.elements = null;
       },
-      _onScroll: function () {
+      trigger: function () {
         var self = this;
-        // this.elements.forEach(function (element) {
-        //   var img = element.querySeletor('img');
-        // })
+        this.elements.forEach(function (element) {
+          var img = element.querySelector('img');
+          self.triggerElement(img);
+        })
+      },
+      hasLoad: function (inElement) {
+        return inElement.dataset.loaded === 'true';
+      },
+      triggerElement: function (inElement) {
+        if (!this.hasLoad(inElement) && isInViewport(inElement, this.offset)) {
+          inElement.dataset.loaded = true;
+          inElement.src = inElement.dataset.src;
+        }
       }
     }
   });
